1494b391f91fe32d7e94847cc6c52c29e3711abf
danilonovaisv <danilo_novais@yahoo.com.br>
2025-12-09 20:38:25 -0300

    update gitignore and remove build artifacts

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"tNiCTMOhoywo8WEIBlCcf40lsg/0sRNbi3FPHMuHbns=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mB9U2jwTesM5uhMEGoCBJXn/0peuUMR9VtuOIpdsnBw=\"\n}"
