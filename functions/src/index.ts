4db9f6849440d640e3bcd372948dacd3d28314ed
danilonovaisv <danilo_novais@yahoo.com.br>
2025-12-13 05:40:33 -0300

    firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,9 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { setGlobalOptions } from 'firebase-functions';
-import { onCall } from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import {setGlobalOptions} from "firebase-functions";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -24,23 +22,4 @@ import * as logger from "firebase-functions/logger";
 // functions should each use functions.runWith({ maxInstances: 10 }) instead.
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
-
-export const yourV2CallableFunction = onCall(
-  {
-    enforceAppCheck: true, // Reject requests with missing or invalid App Check tokens.
-  },
-  (request) => {
-    // request.app contains data from App Check, including the app ID.
-    // Your function logic follows.
-    if (request.app) {
-      logger.info("App Check verified. App ID: " + request.app.appId, { structuredData: true });
-    } else {
-       // This block theoretically won't be reached if enforceAppCheck is true, 
-       // but good for debugging if enforcement is off.
-       logger.warn("App Check not verified", { structuredData: true });
-    }
-    
-    return { message: "Function called successfully with App Check!" };
-  }
-);
+setGlobalOptions({maxInstances: 10});
