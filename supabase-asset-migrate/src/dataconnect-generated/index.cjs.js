d6b4d9fb1540f638a46d43e7e43d54073995d6df
danilonovaisv <danilo_novais@yahoo.com.br>
2026-03-25 16:19:07 -0300

    update

--- supabase-asset-migrate/src/dataconnect-generated/index.cjs.js
+++ supabase-asset-migrate/src/dataconnect-generated/index.cjs.js
@@ -1,28 +1,17 @@
-const {
-  queryRef,
-  executeQuery,
-  mutationRef,
-  executeMutation,
-  validateArgs,
-} = require('firebase/data-connect');
+const { queryRef, executeQuery, mutationRef, executeMutation, validateArgs } = require('firebase/data-connect');
 
 const connectorConfig = {
   connector: 'gemini-test',
   service: 'portfolio-danilo-novais-service',
-  location: 'southamerica-west1',
+  location: 'southamerica-west1'
 };
 exports.connectorConfig = connectorConfig;
 
 const createUserRef = (dcOrVars, vars) => {
-  const { dc: dcInstance, vars: inputVars } = validateArgs(
-    connectorConfig,
-    dcOrVars,
-    vars,
-    true
-  );
+  const { dc: dcInstance, vars: inputVars} = validateArgs(connectorConfig, dcOrVars, vars, true);
   dcInstance._useGeneratedSdk();
   return mutationRef(dcInstance, 'CreateUser', inputVars);
-};
+}
 createUserRef.operationName = 'CreateUser';
 exports.createUserRef = createUserRef;
 
@@ -31,15 +20,10 @@ exports.createUser = function createUser(dcOrVars, vars) {
 };
 
 const getProjectsByUserRef = (dcOrVars, vars) => {
-  const { dc: dcInstance, vars: inputVars } = validateArgs(
-    connectorConfig,
-    dcOrVars,
-    vars,
-    true
-  );
+  const { dc: dcInstance, vars: inputVars} = validateArgs(connectorConfig, dcOrVars, vars, true);
   dcInstance._useGeneratedSdk();
   return queryRef(dcInstance, 'GetProjectsByUser', inputVars);
-};
+}
 getProjectsByUserRef.operationName = 'GetProjectsByUser';
 exports.getProjectsByUserRef = getProjectsByUserRef;
 
@@ -48,15 +32,10 @@ exports.getProjectsByUser = function getProjectsByUser(dcOrVars, vars) {
 };
 
 const createProjectRef = (dcOrVars, vars) => {
-  const { dc: dcInstance, vars: inputVars } = validateArgs(
-    connectorConfig,
-    dcOrVars,
-    vars,
-    true
-  );
+  const { dc: dcInstance, vars: inputVars} = validateArgs(connectorConfig, dcOrVars, vars, true);
   dcInstance._useGeneratedSdk();
   return mutationRef(dcInstance, 'CreateProject', inputVars);
-};
+}
 createProjectRef.operationName = 'CreateProject';
 exports.createProjectRef = createProjectRef;
 
@@ -65,15 +44,10 @@ exports.createProject = function createProject(dcOrVars, vars) {
 };
 
 const deleteProjectRef = (dcOrVars, vars) => {
-  const { dc: dcInstance, vars: inputVars } = validateArgs(
-    connectorConfig,
-    dcOrVars,
-    vars,
-    true
-  );
+  const { dc: dcInstance, vars: inputVars} = validateArgs(connectorConfig, dcOrVars, vars, true);
   dcInstance._useGeneratedSdk();
   return mutationRef(dcInstance, 'DeleteProject', inputVars);
-};
+}
 deleteProjectRef.operationName = 'DeleteProject';
 exports.deleteProjectRef = deleteProjectRef;
 
